3fc1ee4270c33bb58192dc5a65b4d223b6b715dc
Radoslaw Fabisiak <radoslawfabisiak@gmail.com>
2018-12-12 01:07:35 +0100

    Setup basic express app.

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing")
+});
+
+app.get("/measurements", function (req, res){
+    var measurements = [
+        {name: "Radek", weight: 74, bodyfat: 22},
+        {name: "Marek", weight: 84, bodyfat: 27},
+        {name: "Michal", weight: 91, bodyfat: 19}
+    ];
+
+    res.render("measurements", {measurements:measurements})
+});
+
+app.listen(3000, function (){
+    console.log("The bodyfat server has started on port 3000.");
+});
